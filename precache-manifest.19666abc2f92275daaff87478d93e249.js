08e4b52927121f910daf708c8dbe5c807d3fd6ee
abdul ahad <aadee0323@gmail.com>
2019-04-23 14:40:24 +0500

    Updates

---
+++ precache-manifest.19666abc2f92275daaff87478d93e249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbafd42e380057f2fdff",
+    "url": "/myReactPortfolio/static/css/main.a3f99160.chunk.css"
+  },
+  {
+    "revision": "dbafd42e380057f2fdff",
+    "url": "/myReactPortfolio/static/js/main.dbafd42e.chunk.js"
+  },
+  {
+    "revision": "d39266830555e6e8adfb",
+    "url": "/myReactPortfolio/static/js/runtime~main.d3926683.js"
+  },
+  {
+    "revision": "975f2b0a481213369223",
+    "url": "/myReactPortfolio/static/css/2.5745f79c.chunk.css"
+  },
+  {
+    "revision": "975f2b0a481213369223",
+    "url": "/myReactPortfolio/static/js/2.975f2b0a.chunk.js"
+  },
+  {
+    "revision": "4922e49073d53188409996025fd8a2cf",
+    "url": "/myReactPortfolio/index.html"
+  }
+];
